4d23a6c6dfe11acf9e75d76f755f8eb3f082ba07
JuanLucca846 <lucca470@gmail.com>
2024-03-11 14:43:36 -0300

    task-05

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -113,17 +113,58 @@ public class Menu {
 				break;
 			case 3:
 				System.out.println(Cores.TEXT_BLACK_BOLD + "Buscar conta por numero\n\n");
-
+				System.out.println("Digite o número da conta: ");
+				numero = scanner.nextInt();
+				contas.procurarPorNumero(numero);
 				keyPress();
 				break;
 			case 4:
 				System.out.println(Cores.TEXT_BLACK_BOLD + "Atualizar dados da conta\n\n");
+				System.out.println("Digite o número da conta: ");
+				numero = scanner.nextInt();
 
+				var buscarConta = contas.buscarNaCollection(numero);
+
+				if (buscarConta != null) {
+					tipo = buscarConta.getTipo();
+
+					System.out.println("Digite o número da agencia: ");
+					agencia = scanner.nextInt();
+					System.out.println("Digite o nome do titular: ");
+					scanner.skip("\\R?");
+					titular = scanner.nextLine();
+
+					System.out.println("Digite o saldo da conta: ");
+					saldo = scanner.nextFloat();
+
+					switch (tipo) {
+					case 1 -> {
+						System.out.println("Digite o limite de credito (R$): ");
+						limite = scanner.nextFloat();
+
+						contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+					}
+					case 2 -> {
+						System.out.println("Digite o dia de aniversario da conta: ");
+						aniversario = scanner.nextInt();
+
+						contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+					}
+					default -> {
+						System.out.println("Tipo de conta inválido!");
+					}
+					}
+				} else {
+					System.out.println("A conta não foi encontrada!");
+				}
 				keyPress();
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_BLACK_BOLD + "Apagar conta\n\n");
+				System.out.println("Digite o número da conta: ");
+				numero = scanner.nextInt();
 
+				contas.deletar(numero);
 				keyPress();
 				break;
 			case 6:
--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -12,8 +12,14 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
 
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			conta.visualizar();
+		} else {
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
+		}
 	}
 
 	@Override
@@ -33,13 +39,27 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
+		var buscarConta = buscarNaCollection(conta.getNumero());
 
+		if (buscarConta != null) {
+			listaContas.set(listaContas.indexOf(buscarConta), conta);
+			System.out.println("\nA conta número: " + conta.getNumero() + " foi atualizaada com sucesso!");
+		} else {
+			System.out.println("\nA conta número: " + conta.getNumero() + " não foi encontrada!");
+		}
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			if (listaContas.remove(conta) == true) {
+				System.out.println("\nA conta número: " + numero + " foi deletada com sucesso!");
+			}
+		} else {
+			System.out.println("\nA conta numero: " + numero + " não foi encontrada!");
+		}
 
 	}
 
@@ -63,6 +83,15 @@ public class ContaController implements ContaRepository {
 
 	public int gerarNumero() {
 		return ++numero;
+	}
+
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		return null;
 	}
 
 }
